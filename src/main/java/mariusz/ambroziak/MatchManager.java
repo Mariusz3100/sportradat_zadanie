bde3f8eb3d34f1e9f63e02e487b089d68d0e65d5
Mariusz Ambroziak <mariuszambroziak667@gmail.com>
2025-04-11 11:57:59 +0200

    Framework implementation of MatchManager, that will contain core logic of the app.

---
+++ src/main/java/mariusz/ambroziak/MatchManager.java
@@ -0,0 +1,24 @@
+package mariusz.ambroziak;
+
+import java.util.List;
+
+public class MatchManager {
+
+    public void startGame(String homeTeam,String awayTeam){
+        //TODO
+    }
+
+    public void finishGame(String homeTeam,String awayTeam){
+        //TODO
+    }
+
+    public void updateScore(int homeTeamScore,int awayTeamScore){
+        //TODO
+    }
+
+    public List<String> summaryOfMatchesByTotalScore(){
+        //TODO
+        return null;
+    }
+
+}
