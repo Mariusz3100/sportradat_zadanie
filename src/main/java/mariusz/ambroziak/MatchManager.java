e8a9d347897117fdba34d38b5740cce2875778e8
Mariusz Ambroziak <mariuszambroziak667@gmail.com>
2025-04-14 01:34:36 +0200

    First refactor, mostly clean code changes.

--- src/main/java/mariusz/ambroziak/MatchManager.java
+++ src/main/java/mariusz/ambroziak/MatchManager.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MatchManager {
+    public static final String TEAMS_AND_SCORE_SPLITER_IN_UPDATE = ":";
+    public static final String TEAMS_SPLITTER_IN_UPDATE = "-";
+    public static final String SCORES_SPLITTER_IN_UPDATE = "–";
+    public static final String FORMAT_OF_MATCH_FOR_SUMMARY = "%s %d - %s %d";
+
     Map<Match,MatchResult> currentMatches=new HashMap<>();
 
     public void startGame(String homeTeam,String awayTeam){
@@ -17,23 +22,39 @@ public class MatchManager {
     }
 
     public void updateScore(String update) {
-        String[] updateSplit=update.split(":");
-        String[] teamsSplit=updateSplit[0].split("-");
-        String[] scoreSplit=updateSplit[1].split("–");
-        Match teams=new Match(teamsSplit[0].trim(),teamsSplit[1].trim());
-        MatchResult updatedResult=new MatchResult(Integer.parseInt(scoreSplit[0].trim()),Integer.parseInt(scoreSplit[1].trim()));
+        String[] updateSplit=update.split(TEAMS_AND_SCORE_SPLITER_IN_UPDATE);
+        Match teams = parseTeamsPart(updateSplit[0]);
+        MatchResult updatedResult = parseScorePart(updateSplit[1]);
         currentMatches.put(teams,updatedResult);
 
     }
 
+    private static Match parseTeamsPart(String updateSplit) {
+        String[] teamsSplit= updateSplit.split(TEAMS_SPLITTER_IN_UPDATE);
+        Match teams=new Match(teamsSplit[0].trim(),teamsSplit[1].trim());
+        return teams;
+    }
+
+    private static MatchResult parseScorePart(String scorePart) {
+        String[] scoreSplit= scorePart.split(SCORES_SPLITTER_IN_UPDATE);
+        int homeTeamScore = Integer.parseInt(scoreSplit[0].trim());
+        int awayTeamScore = Integer.parseInt(scoreSplit[1].trim());
+        return new MatchResult(homeTeamScore, awayTeamScore);
+    }
+
     public String summaryOfMatchesByTotalScore(){
-        String pattern="%s %d - %s %d";
         for(Map.Entry<Match,MatchResult> entry:currentMatches.entrySet()){
             Match match=entry.getKey();
             MatchResult result=entry.getValue();
-            return String.format(pattern,match.homeTeamName(),result.homeTeamScore(),match.awayTeamName(),result.awayTeamScore());
+            return summaryOfOneMatch(match, result);
         }
         return "";
+    }
+
+    private static String summaryOfOneMatch(Match match, MatchResult result) {
+        return String.format(FORMAT_OF_MATCH_FOR_SUMMARY,
+                match.homeTeamName(), result.homeTeamScore(),
+                match.awayTeamName(), result.awayTeamScore());
     }
 
 }
