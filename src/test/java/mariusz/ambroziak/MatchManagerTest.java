4dbf852c44329926d283435f3ee343cabfd13e8e
Mariusz Ambroziak <mariuszambroziak667@gmail.com>
2025-04-14 10:00:14 +0200

    Second set of tests. This time some edge cases.

--- src/test/java/mariusz/ambroziak/MatchManagerTest.java
+++ src/test/java/mariusz/ambroziak/MatchManagerTest.java
@@ -29,16 +29,64 @@ class MatchManagerTest {
 
     }
 
+    @Test
+    void testTwoGamesPlaying() {
+        manager.startGame("Mexico","Canada");
+        manager.startGame("Spain","Brazil");
+
+        String expectedResult = """
+                    Mexico 0 - Canada 0
+                    Spain 0 - Brazil 0""";
+        assertEquals(expectedResult,manager.summaryOfMatchesByTotalScore());
+
+    }
+
 
     @org.junit.jupiter.api.Test
     void updateScore() {
         manager.startGame("Mexico","Canada");
         manager.updateScore("Mexico - Canada: 0 – 5");
+
         assertEquals("Mexico 0 - Canada 5",manager.summaryOfMatchesByTotalScore());
     }
 
     @org.junit.jupiter.api.Test
+    void updateScoreForNotExistinghGame() {
+        assertThrows(IllegalStateException.class,()->manager.updateScore("Mexico - Canada: 0 – 5"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void finishNotExistinghGame() {
+        assertThrows(IllegalStateException.class,()->manager.finishGame("Mexico","Canada"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void startAlreadyExistinghGame() {
+        manager.startGame("Germany","France:");
+        assertThrows(IllegalStateException.class,()->manager.startGame("Germany","France:"));
+    }
+
+    @org.junit.jupiter.api.Test
+    void startAndFinishGame() {
+        manager.startGame("Mexico","Canada");
+        manager.finishGame("Mexico","Canada");
+
+        assertEquals("",manager.summaryOfMatchesByTotalScore());
+    }
+
+
+    @org.junit.jupiter.api.Test
     void updateScoreAndFinishGame() {
+        manager.startGame("Mexico","Canada");
+        manager.updateScore("Mexico - Canada: 0 – 5");
+        manager.finishGame("Mexico","Canada");
+
+        assertEquals("",manager.summaryOfMatchesByTotalScore());
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void startAndScoreupdateScoreAndFinishGame() {
         manager.startGame("Mexico","Canada");
         manager.updateScore("Mexico - Canada: 0 – 5");
         manager.finishGame("Mexico","Canada");
