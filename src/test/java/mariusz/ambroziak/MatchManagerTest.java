d55ef09acabd919ffc71c7a20a11b50470862d55
Mariusz Ambroziak <mariuszambroziak667@gmail.com>
2025-04-15 09:58:12 +0200

    Third set of tests (one test exactly as in pdf, plus one old test corrected)

--- src/test/java/mariusz/ambroziak/MatchManagerTest.java
+++ src/test/java/mariusz/ambroziak/MatchManagerTest.java
@@ -35,8 +35,8 @@ class MatchManagerTest {
         manager.startGame("Spain","Brazil");
 
         String expectedResult = """
-                    Mexico 0 - Canada 0
-                    Spain 0 - Brazil 0""";
+                    Spain 0 - Brazil 0
+                    Mexico 0 - Canada 0""";
         assertEquals(expectedResult,manager.summaryOfMatchesByTotalScore());
 
     }
@@ -45,6 +45,18 @@ class MatchManagerTest {
     @org.junit.jupiter.api.Test
     void updateScore() {
         manager.startGame("Mexico","Canada");
+        manager.updateScore("Mexico - Canada: 0 – 5");
+
+        assertEquals("Mexico 0 - Canada 5",manager.summaryOfMatchesByTotalScore());
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void updateScoreManyTimes() {
+        manager.startGame("Mexico","Canada");
+        manager.updateScore("Mexico - Canada: 0 – 1");
+        manager.updateScore("Mexico - Canada: 0 – 2");
+        manager.updateScore("Mexico - Canada: 0 – 3");
         manager.updateScore("Mexico - Canada: 0 – 5");
 
         assertEquals("Mexico 0 - Canada 5",manager.summaryOfMatchesByTotalScore());
@@ -62,8 +74,25 @@ class MatchManagerTest {
 
     @org.junit.jupiter.api.Test
     void startAlreadyExistinghGame() {
-        manager.startGame("Germany","France:");
-        assertThrows(IllegalStateException.class,()->manager.startGame("Germany","France:"));
+        manager.startGame("Germany","France");
+        assertThrows(IllegalStateException.class,()->manager.startGame("Germany","France"));
+    }
+
+
+
+    @org.junit.jupiter.api.Test
+    void startTwoGamesUpdateInDifferentOrder() {
+        manager.startGame("Germany","France");
+        manager.startGame("Uruguay","Italy");
+
+        manager.updateScore("Uruguay - Italy: 6 – 6");
+        manager.updateScore("Germany - France: 2 – 2");
+
+
+        String expectedResult = """
+                    Uruguay 6 - Italy 6
+                    Germany 2 - France 2""";
+        assertEquals(expectedResult,manager.summaryOfMatchesByTotalScore());
     }
 
     @org.junit.jupiter.api.Test
@@ -86,12 +115,45 @@ class MatchManagerTest {
 
 
     @org.junit.jupiter.api.Test
-    void startAndScoreupdateScoreAndFinishGame() {
+    void startAndUpdateScoreAndFinishGame() {
         manager.startGame("Mexico","Canada");
         manager.updateScore("Mexico - Canada: 0 – 5");
         manager.finishGame("Mexico","Canada");
 
         assertEquals("",manager.summaryOfMatchesByTotalScore());
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void startScoreAndUpdateManyGames() {
+        manager.startGame("Mexico","Canada");
+        manager.updateScore("Mexico - Canada: 0 – 5");
+        manager.startGame("Spain","Brazil");
+        manager.updateScore("Spain - Brazil: 1 – 0");
+        manager.updateScore("Spain - Brazil: 2 – 0");
+        manager.updateScore("Spain - Brazil: 5 – 2");
+        manager.updateScore("Spain - Brazil: 10 – 2");
+        manager.startGame("Germany","France");
+        manager.updateScore("Germany - France: 2 – 0");
+        manager.updateScore("Germany - France: 2 – 1");
+        manager.updateScore("Germany - France: 2 – 2");
+        manager.startGame("Uruguay","Italy");
+        manager.updateScore("Uruguay - Italy: 0 – 0");
+        manager.updateScore("Uruguay - Italy: 2 – 2");
+        manager.updateScore("Uruguay - Italy: 6 – 6");
+        manager.startGame("Argentina","Australia");
+        manager.updateScore("Argentina - Australia: 1 – 1");
+        manager.updateScore("Argentina - Australia: 1 – 1");
+        manager.updateScore("Argentina - Australia: 3 – 1");
+
+        String expectedResult = """
+                Uruguay 6 - Italy 6
+                Spain 10 - Brazil 2
+                Mexico 0 - Canada 5
+                Argentina 3 - Australia 1
+                Germany 2 - France 2""";
+
+        assertEquals(expectedResult,manager.summaryOfMatchesByTotalScore());
     }
 
 
